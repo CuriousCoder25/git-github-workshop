13c17eeee9e94a4e440ded19dabd06a47b27ba08
curiouscoder <gauravkathayat12@gmail.com>
2026-06-07 09:00:00 +0545

    changes

---
+++ demo.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    int num;// new add file
+    printf("Enter a number::");
+    scanf("%d",&num);//memory address
+    int product = num *num; // git local db -- commit 
+
+}
